2a89dcf266be48808e60a0def1fd433c5b7a7c61
Danish <dan71ish@gmail.com>
2024-12-09 15:31:15 +0530

    implemented authentication using next-auth and also added the middleware

--- app/lib/auth/authOptions.ts
+++ app/lib/auth/authOptions.ts
@@ -1,15 +1,52 @@
 import GoogleProvider from "next-auth/providers/google";
+import { db } from "../db";
+import { users } from "../db/schema";
+import { AuthOptions } from "next-auth";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       async profile(profile, token: any) {
-        console.log(profile);
-        console.log(token);
-        return { id: profile.sub };
+        const data = {
+          fname: profile.given_name,
+          lname: profile.family_name,
+          email: profile.email,
+          provider: "GOOGLE",
+          external_id: profile.sub,
+          image: profile.picture,
+        };
+        try {
+          const user = await db
+            .insert(users)
+            .values(data)
+            .onConflictDoUpdate({ target: users.email, set: data })
+            .returning();
+
+          return {
+            ...data,
+            name: data.fname,
+            id: String(user[0].id),
+            role: user[0].role,
+          };
+        } catch (error) {
+          console.log(error);
+          return { id: "" };
+        }
       },
     }),
   ],
+  callbacks: {
+    session(data: any) {
+      return data;
+    },
+    jwt({ token, user }: { token: any; user: any }) {
+      if (user) {
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
 };
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
 import { type Config } from "drizzle-kit";
 
 export default {
-  schema: "./app/db/schema.ts",
+  schema: "./app/lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-  tablesFilter: ["job-portal_*"],
 } satisfies Config;
